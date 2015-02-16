90e7548f223dfc27f483074627ee47db7ed2b625
Morglod <morglod@gmail.com>
2015-02-16 17:16:16 +0400

    Will be deleted

--- Shaders/ShaderFeatureSet.hpp
+++ Shaders/ShaderFeatureSet.hpp
@@ -1,19 +1,28 @@
 #pragma once
 
+#include "../Textures/TextureInterfaces.hpp"
+
+#include <string>
+
 namespace mr {
 
 class ShaderFeatureSet {
 public:
-    /*
-        If false, sampler2D
-        If true, nv bindless texture handle
-    */
-    bool nvBindless = false;
+    bool bindlessTextures = false;
 
     /*
         Pack all uniforms as uniform blocks and store in buffers.
+        Not supported
     */
-    bool uniformBlocks = false;
+    //bool uniformBlocks = false;
+
+    std::string ShaderHeader();
+    std::string TextureHandleUniform(std::string uniformName, mr::ITexture::Types const& textureType);
+
+    /*
+        Bind texture to unit if !bindlessTextures
+    */
+    bool SetUniformTexture(ITexture* tex, class IShaderUniform* shaderUniform);
 };
 
 }
